9580716418a969c5cfd45871fa943533e6b0c176
adrilene <adrilenefonseca@gmail.com>
2020-12-18 12:13:49 -0300

    new architecture

--- project/communication/client_smp.py
+++ project/communication/client_smp.py
@@ -1,6 +1,7 @@
 from project.services.smartphone_service import insert_data
 import threading
 import paho.mqtt.client as mqtt
+from time import sleep
 import json
 
 
@@ -8,61 +9,10 @@ class ClientSMP:
     def __init__(self):
         self.client = mqtt.Client("smp")
         self.client.connect(host="dojot.atlantico.com.br", port=1883)
-        self.client.on_message = self.callback
         self.confirmation = False
 
-    def publish_to_bm(self, data):
-        data["from"] = "smp"
-        data["to"] = "bm"
-        self.client.publish("/gesad/9e4ed4/attrs", payload=json.dumps(data))
+    #send to smp in dojot
+    def publish_to_smp(self, confirmation):
+        data = {'msg': 'Confirmation Received' if confirmation else 'Confirmation not received'}
+        self.client.publish("/gesad/434339/attrs", payload=json.dumps(data))
 
-    def publish_to_tv(self, data):
-        data["from"] = "smp"
-        data["to"] = "tv"
-        self.client.publish("/gesad/f0bf6d/attrs", payload=json.dumps(data))
-
-    def subscribe(self):
-        self.client.subscribe("/gesad/434339/attrs")
-
-    def callback(self, msg):
-        body = json.loads(msg.payload)
-        if body["from"] == "bm":
-            self.bm_message_received(body)
-
-        elif body["from"] == "tv":
-            self.tv_message_received(body)
-
-    def bm_message_received(self, body):
-        print(f'from BM {body}' )
-        # insert_data(body)
-        if body["type"] == "notification":
-            print("Received Notification")
-            self.confirmation = False
-            threading.Thread(target=self.wait_for_confirmation, args=(body,)).start()
-            return "ok"
-
-        if body["type"] == "status":
-            return "ok"
-
-    def tv_message_received(self, body):
-        print(f'from TV {body}')
-        if "unlocked" in body["msg"]:
-            self.confirmation = True
-            data = {
-                "type": "confirmation",
-                "msg": "The notification is confirmed",
-                "from": "smp",
-                "to": "bm",
-            }
-            self.publish_to_bm(data)
-            return "Confirmation Sent"
-
-    def wait_for_confirmation(self, data):
-        time = 0
-        while not client_smp.confirmation:
-            print(f"I'm waiting for {time} seconds.")
-            if time >= 7:
-                client_smp.publish_to_tv(data)
-                break
-            sleep(1)
-            time += 1
--- project/controllers/main_controller.py
+++ project/controllers/main_controller.py
@@ -1,29 +1,45 @@
 from project import app
 from flask import request, jsonify
 from project.communication.client_smp import ClientSMP
+import threading
+from time import sleep
 
 
 client_smp = ClientSMP()
 client_smp.subscribe()
+confirmation = False
 
+def wait_for_confirmation(self):
+    global confirmation, client_smp
+
+    time = 0
+    while not confirmation:
+        print(f"I'm waiting for {time} seconds.")
+        if time >= 7:
+            client_smp.publish_to_smp(confirmation)
+            break
+        sleep(1)
+        time += 1
 
 @app.route("/", methods=["GET"])
 def check():
     return "I'm working Smartphone"
 
 
-@app.route("/smp_send", methods=["GET"])
-def send_bm():
+@app.route("/smp_receive_confirmation", methods=["GET"])
+def smp_receive_confirmation():
+    global client_smp, confirmation
+    
+    confirmation = True
+    client_smp.publish_to_smp(confirmation)
+
+    return "OK"
+
+
+@app.route("/smp_receive_notification", methods=["GET"])
+def smp_receive_notification():
     global client_smp
     
-    client_smp.confirmation = True
-
-    body = {
-        "type": "confirmation",
-        "msg": "The notification is confirmed",
-        "from": "smp",
-        "to": "bm",
-    }
-    client_smp.publish_to_bm(body)
-
+    threading.Thread(target=wait_for_confirmation).start()
+    
     return "OK"
